ea51926a634402745ec696192466bda3172ecca1
Samantha Webb <samanthawebb@outlook.com>
2016-02-05 20:36:29 -0500

    add array of presidents to variable named presidents to main.js

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var presidents = [
+    { firstName:'Barack', lastName:'Obama', termLength:2, party:'Democrat' },
+    { firstName:'George', lastName:'Bush', termLength:2, party:'Republican' },
+    { firstName:'Bill', lastName:'Clinton', termLength:2, party:'Democrat' },
+    { firstName:'George', lastName:'Bush', termLength:1, party:'Republican' },
+    { firstName:'Ronald', lastName:'Reagan', termLength:2, party:'Republican' }
+];
