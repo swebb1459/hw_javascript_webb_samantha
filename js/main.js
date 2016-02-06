26d87e08523094a25f5e97b0bc8f9a76bac6b402
Samantha Webb <samanthawebb@outlook.com>
2016-02-05 20:41:13 -0500

    add console.log calling president to main.js

--- js/main.js
+++ js/main.js
@@ -5,3 +5,5 @@ var presidents = [
     { firstName:'George', lastName:'Bush', termLength:1, party:'Republican' },
     { firstName:'Ronald', lastName:'Reagan', termLength:2, party:'Republican' }
 ];
+
+console.log( presidents[2].firstName + ' ' + presidents[2].lastName );
