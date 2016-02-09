9314fe92cef9c3e66376794092b3649b4b80913e
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-08 19:25:31 -0800

    put each object's key and value pairs on their own line

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
 var presidents = [
-    { firstName:'Barack', lastName:'Obama', termLength:2, party:'Democrat' },
-    { firstName:'George', lastName:'Bush', termLength:2, party:'Republican' },
-    { firstName:'Bill', lastName:'Clinton', termLength:2, party:'Democrat' },
-    { firstName:'George', lastName:'Bush', termLength:1, party:'Republican' },
-    { firstName:'Ronald', lastName:'Reagan', termLength:2, party:'Republican' }
+    {
+      firstName:'Barack',
+      lastName:'Obama',
+      termLength:2,
+      party:'Democrat'
+    },
+    {
+      firstName:'George',
+      lastName:'Bush',
+      termLength:2,
+      party:'Republican'
+    },
+    {
+      firstName:'Bill',
+      lastName:'Clinton',
+      termLength:2,
+      party:'Democrat'
+    },
+    {
+      firstName:'George',
+      lastName:'Bush',
+      termLength:1,
+      party:'Republican'
+    },
+    {
+      firstName:'Ronald',
+      lastName:'Reagan',
+      termLength:2,
+      party:'Republican'
+    }
 ];
 
 console.log( presidents[2].firstName + ' ' + presidents[2].lastName );
